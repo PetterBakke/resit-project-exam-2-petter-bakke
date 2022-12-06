ee224e437ba3cc7350cec3e11b6b795e0e05638e
PetterBakke <74598862+PetterBakke@users.noreply.github.com>
2022-12-06 22:53:09 +0100

    fixed bugs on pagedetail

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -1,64 +1,7 @@
-// import { useState, useEffect } from 'react';
-// import { useParams } from 'react-router-dom';
-// import axios from 'axios';
 import { Details_url, BASE_URI } from "../../constants/api";
 import { AiOutlineHeart } from "react-icons/ai";
-
-// function PageDetail() {
-//   const [product, setProduct] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-
-//   const { id } = useParams();
-
-//   const url = Details_url + id + "?populate=*";
-//   console.log(url);
-
-// const imagePath = `${BASE_URI}${product.attributes.image.data[0].attributes.url}`;
-
-//   useEffect(() => {
-//     axios.get(url)
-//       .then((response) => {
-//         setProduct(response.data);
-//         console.log(response.data);
-//         setLoading(false);
-//       })
-//       .catch(function (err) {
-//         setError(err);
-//       });
-//   }, [url]);
-
-//   if (loading) {
-//     return <div>Loading...</div>;
-//   }
-
-//   if (error) {
-//     return <div>Error: {error}</div>;
-//   }
-
-
-
-//   return (
-//     <>
-//       <div className='page-detail'>
-//         <div>
-//         <img src={imagePath} alt="This is the product cover" className="product-img" />
-//           <AiOutlineHeart className="fav-button" />
-//           <p key={product.attributes.title} className="p-title">{product.attributes.title}</p>
-//         </div>
-//         <div className='subgrid'>
-//           <h5 className='heading-description'>{product.attributes.title}</h5>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default PageDetail;
-
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-// import { API_URL } from "../../constants/api";
 import Container from 'react-bootstrap/Container';
 
 function PageDetail() {
